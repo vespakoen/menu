9b436289effede9c0cddc39c1ed9093bd05aa1ca
Anahkiasen <ehtnam6@gmail.com>
2013-01-31 00:11:06 +0100

    Add renderTabbed helper to MenuObject class

--- src/Menu/Traits/MenuObject.php
+++ src/Menu/Traits/MenuObject.php
@@ -9,5 +9,20 @@ namespace Menu;
 
 abstract class MenuObject
 {
+  ////////////////////////////////////////////////////////////////////
+  ////////////////////////////// HELPERS /////////////////////////////
+  ////////////////////////////////////////////////////////////////////
 
+  /**
+   * Renders content in a new line, tabbed
+   *
+   * @param string  $content The content to render
+   * @param integer $tabs    The number of tabs
+   *
+   * @return string
+   */
+  private function renderTabbed($content, $tabs = 0)
+  {
+    return PHP_EOL.str_repeat("\t", $tabs).$content;
+  }
 }
