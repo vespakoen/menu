c8193db94641ede1233ec2c72158b67bf56ddbdf
Anahkiasen <ehtnam6@gmail.com>
2013-01-30 17:47:44 +0100

    Add Travis file and basic test

--- tests/MenuTest.php
+++ tests/MenuTest.php
@@ -1,5 +1,12 @@
 <?php
+use Menu\Menu;
+
 class MenuTest extends PHPUnit_Framework_TestCase
 {
+  public function testCanGetAMenu()
+  {
+    $menu = Menu::handler('foo');
 
+    $this->assertInstanceOf('Menu\MenuHandler', $menu);
+  }
 }
