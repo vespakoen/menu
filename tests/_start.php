2ab7c3a47f8c7d03fbc20b7845852a80962c8daf
Anahkiasen <ehtnam6@gmail.com>
2013-01-31 00:29:38 +0100

    Add assertHTML helper to help testing HTML

--- tests/_start.php
+++ tests/_start.php
@@ -7,4 +7,24 @@ abstract class MenuTests extends PHPUnit_Framework_TestCase
   {
     Menu::reset();
   }
+
+  ////////////////////////////////////////////////////////////////////
+  ////////////////////////////// HELPERS /////////////////////////////
+  ////////////////////////////////////////////////////////////////////
+
+  /**
+   * Enhanced version of assertTag
+   *
+   * @param array  $matcher The tag matcher
+   * @param string $html    The HTML
+   */
+  protected function assertHTML($matcher, $html)
+  {
+    $this->assertTag(
+      $matcher,
+      $input,
+      "Failed asserting that the HTML matches the provided format :\n\t"
+        .$input."\n\t"
+        .json_encode($matcher));
+  }
 }
