7a4ec71f4b469d5b8b38de5804f967e95b2e20df
Anahkiasen <ehtnam6@gmail.com>
2013-01-31 00:08:55 +0100

    Add canvas for MenuObject for future work

---
+++ src/Menu/Traits/MenuObject.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * MenuObject
+ *
+ * Allows dynamic setting and getting of attributes
+ * on the various parts of a menu (items, itemlists, etc)
+ */
+namespace Menu;
+
+abstract class MenuObject
+{
+
+}
