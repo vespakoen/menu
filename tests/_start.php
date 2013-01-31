1f65fb696dc350489726c14d8e5c54cbc0fbc3a0
Anahkiasen <ehtnam6@gmail.com>
2013-01-31 14:11:01 +0100

    Fix recursive bug

--- tests/_start.php
+++ tests/_start.php
@@ -49,7 +49,7 @@ abstract class MenuTests extends PHPUnit_Framework_TestCase
    */
   protected function matchListWithItem()
   {
-    $list = $this->matchListWithItem();
+    $list = $this->matchList();
     $list['child'] = $this->matchItem();
 
     return $list;
