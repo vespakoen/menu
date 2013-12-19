18da3f484204e5717770e62fec612b4a6d9f3eb9
nostalgie <Nostalgie@users.noreply.github.com>
2013-12-19 12:46:44 +0300

    Update MenuServiceProvider.php
    Set local menu support for laravel4

--- src/Menu/MenuServiceProvider.php
+++ src/Menu/MenuServiceProvider.php
@@ -20,9 +20,11 @@ class MenuServiceProvider extends ServiceProvider
    */
   public function register()
   {
+    
+    $this->package('vespakoen/menu', null, __DIR__ . '/../');
     $container = Menu::getContainer();
     $container['url'] = $this->app['url'];
-
+    $container['config'] = $this->app['config'];
     Menu::setContainer($container);
   }
 
