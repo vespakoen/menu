5b1c177ec6c72c75397925c5f8c64f3da1928862
Michael <mikemckend@gmail.com>
2015-02-07 11:13:43 -0800

    Removed deprecated package call

--- src/Menu/MenuServiceProvider.php
+++ src/Menu/MenuServiceProvider.php
@@ -23,8 +23,6 @@ class MenuServiceProvider extends ServiceProvider
    */
   public function register()
   {
-    $this->package('vespakoen/menu', null, __DIR__ . '/../');
-
     $container = Menu::getContainer();
     $container['url'] = $this->app['url'];
     $container['config'] = $this->app['config'];
